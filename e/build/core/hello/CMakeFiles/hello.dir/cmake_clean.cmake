025dcde2b051d13fe5205a7a5a28d20b55f1a8d0
Samuel <sunzuolei@gmail.com>
2015-04-29 22:36:03 +0800

    Finish project e

---
+++ e/build/core/hello/CMakeFiles/hello.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello.dir/src/sayHello.cpp.o"
+  "../../../lib/libhello.pdb"
+  "../../../lib/libhello.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ e/build/core/sqrt/CMakeFiles/sqrt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sqrt.dir/src/sqrt.cpp.o"
+  "../../../lib/libsqrt.pdb"
+  "../../../lib/libsqrt.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sqrt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
