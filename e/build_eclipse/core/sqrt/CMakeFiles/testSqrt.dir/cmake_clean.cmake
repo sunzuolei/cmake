bd2b02da5dbf6ea797a532928815e7bcd8886686
WYue2015 <1805515068@qq.com>
2015-09-18 19:22:22 +0800

    add e directory

---
+++ e/build_eclipse/core/sqrt/CMakeFiles/testSqrt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/testSqrt.dir/test/testSqrt.cpp.o"
+  "../../bin/testSqrt.pdb"
+  "../../bin/testSqrt"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/testSqrt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ e/build_eclipse/core/sqrt/CMakeFiles/testSqrt.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for testSqrt.
+# This may be replaced when dependencies are built.
