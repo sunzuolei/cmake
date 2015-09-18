68d1f065307e3f58c1c127ca4c293bdc98530062
WYue2015 <1805515068@qq.com>
2015-09-19 00:48:01 +0800

    remove c

---
+++ c/build_eclipse/example/CMakeFiles/sqrtDemoC.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/sqrtDemoC.dir/__/core/sqrt.cpp.o"
+  "CMakeFiles/sqrtDemoC.dir/demo.cpp.o"
+  "../bin/sqrtDemoC.pdb"
+  "../bin/sqrtDemoC"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/sqrtDemoC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ c/example/demo.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "../core/sqrt.h"
+int main()
+{
+	double b=25.0;
+	double a=0.0;
+	       a=getSqrt(b);
+	printf("\n");
+	printf("**************************\n");
+	printf("I am demo c of MPIG0024:\na is %.lf, b is %.lf\n",a,b);
+	printf("**************************\n");
+	printf("\n");
+	return 0;
+}
