1e8bb284eb0dc1db39a54c680d65de11b9e4a0a1
WYue2015 <1805515068@qq.com>
2015-09-18 20:07:56 +0800

    add eclipse and qt project for demoA

---
+++ a/build_eclipse/CMakeFiles/sqrtDemoA.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/sqrtDemoA.dir/demo.cpp.o"
+  "CMakeFiles/sqrtDemoA.dir/sqrt.cpp.o"
+  "bin/sqrtDemoA.pdb"
+  "bin/sqrtDemoA"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/sqrtDemoA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
