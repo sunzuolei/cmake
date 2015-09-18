b9a4db666b6623b5a7c20a2c10b59ee94a42a1c9
WYue2015 <1805515068@qq.com>
2015-09-18 20:14:26 +0800

    add eclipse and qt project for demoE

---
+++ e/build_eclipse/core/hello/CMakeFiles/testHello.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/testHello.dir/test/testHello.cpp.o"
+  "../../bin/testHello.pdb"
+  "../../bin/testHello"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/testHello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
